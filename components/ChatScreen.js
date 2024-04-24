8d5a5bb729e60f649eae73d341e22fbdbb296c60
dayu <dayu.hidayatullah@idas.id>
2024-04-24 11:13:58 +0700

    reset

--- components/ChatScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-export default function ChatScreen() {
-  return <Container>ChatScreen</Container>;
-}
-
-const Container = styled.div``;
